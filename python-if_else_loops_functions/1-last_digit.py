a985f335fdc5a763d6439a12b803a0fa6b75706f
JulienGirardey <juliengirardey2@gmail.com>
2025-05-06 09:14:39 +0200

    adapt the code to pass pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-	print(f"Last digit of {number} is {number % 10}", end= " ")
+    print(f"Last digit of {number} is {number % 10}", end=" ")
 else:
-	print(f"Last digit of {number} is {number * -1 % 10}", end= " ")
+    print(f"Last digit of {number} is {number * -1 % 10}", end=" ")
 if number % 10 > 5:
-	print("and is greater than 5")
+    print("and is greater than 5")
 elif number % 10 < 5:
-	print("and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
 else:
-	print("and is 0")
+    print("and is 0")
