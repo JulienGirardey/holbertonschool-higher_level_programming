c54fe74e34c1149eb964327bd8d3006a68282dbc
JulienGirardey <juliengirardey2@gmail.com>
2025-05-06 15:39:44 +0200

    add file to prints all possible different combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for i in range(0, 10):
     for j in range(i + 1, 10):
         if i == 0 and j == 1:
             print(f"{i}{j}", end="")
-            break
+            continue
         if i == j:
             continue
         print(f", {i}{j}", end="")
