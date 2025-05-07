a9da29416e09e98728722abcf8237bd3a59eed20
JulienGirardey <juliengirardey2@gmail.com>
2025-05-07 15:28:11 +0200

    that add elements to the list and display the numbers of elements

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 import sys
 
 if __name__ == "__main__":
-    print("{} argument{}".format(len(sys.argv) - 1, "."
-                                 if len(sys.argv) == 1 else ":"))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+
+    argv = sys.argv
+
+    argc = len(argv) - 1
+    print("{} argument{}".format(argc, "." if argc == 0 else ":"))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
