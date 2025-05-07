1652da4051e81a0060b47bae2a7ae5a186875972
JulienGirardey <juliengirardey2@gmail.com>
2025-05-07 12:05:49 +0200

    that say if the last digit of number is better or less than 5

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-    print("Last digit of {} is {}".format(number, number % 10), end=" ")
+    digit = number % 10
+    print("Last digit of {} is {}".format(number, digit), end=" ")
 else:
-    print("Last digit of {} is {}".format(number, number * -1 % 10), end=" ")
-if number % 10 > 5:
-    print("and is greater than 5")
-elif number % 10 < 5:
-    print("and is less than 6 and not 0")
-else:
+    digit = number * -1 % 10
+    digit = digit - digit * 2
+    print("Last digit of {} is {}".format(number, digit), end=" ")
+if digit == 0:
     print("and is 0")
+elif digit > 5:
+    print("and is greater than 5")
+else:
+    print("and is less than 6 and not 0")
