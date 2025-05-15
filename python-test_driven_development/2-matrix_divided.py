454225437301ea6719451cae77fc319dd5a44a21
JulienGirardey <juliengirardey2@gmail.com>
2025-05-15 19:07:19 +0200

    that divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,12 +18,12 @@ def matrix_divided(matrix, div):
         if not len(row_check) == len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
 
+    if not isinstance(matrix, (list)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not isinstance(matrix, (list)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     result = []
     for row in matrix:
