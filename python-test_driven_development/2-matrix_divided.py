38d560032954272c97693e6eb48c88ed1fd9bca4
JulienGirardey <juliengirardey2@gmail.com>
2025-05-16 09:04:38 +0200

    that divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Function that divides all elements of a matrix by a given number.
-Validates the structure of the matrix and the type of each element.
-Ensures all rows are of equal size and that the divisor is a number.
-Raises TypeError or ZeroDivisionError with precise messages when needed.
-Returns a new matrix with each result rounded to 2 decimal places."""
+"""
+matrix_divided.py
+This module provides a function to divide
+all elements of a matrix by a given number.
+"""
 
 
 def matrix_divided(matrix, div):
-    """Function that divides all elements of a matrix by a given number.
-Validates the structure of the matrix and the type of each element.
-Ensures all rows are of equal size and that the divisor is a number.
-Raises TypeError or ZeroDivisionError with precise messages when needed.
-Returns a new matrix with each result rounded to 2 decimal places."""
+    """Divides all elements of a matrix by
+    a given number, rounded to 2 decimal places.
+    """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
@@ -21,10 +20,12 @@ Returns a new matrix with each result rounded to 2 decimal places."""
 
     for row_check in matrix:
         if not isinstance(row_check, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix"
+                            "(list of lists) of integers/floats")
         for elem_check in row_check:
             if not isinstance(elem_check, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix"
+                                "(list of lists) of integers/floats")
         if not len(row_check) == len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
 
