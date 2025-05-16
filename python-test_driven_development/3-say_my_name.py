ede93a5aace4a10dfbc4ddcfb0d8f1bab193fdc4
JulienGirardey <juliengirardey2@gmail.com>
2025-05-16 10:54:32 +0200

    that print my name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,11 +7,11 @@ That prints My name is <first name> <last name>
 
 
 def say_my_name(first_name, last_name=""):
-	"""
-	This function handle wrong type name and other Error
-	"""
-	if not isinstance(first_name, str):
-		raise TypeError("first_name must be a string")
-	if not isinstance(last_name, str):
-		raise TypeError("last_name must be a string")
-	print("My name is {} {}".format(first_name, last_name))
+    """
+    This function handle wrong type name and other Error
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
