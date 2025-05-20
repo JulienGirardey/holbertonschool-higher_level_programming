2746582daa09560e8e84b1ea2f2f7480404bab0d
JulienGirardey <juliengirardey2@gmail.com>
2025-05-20 14:41:52 +0200

    that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-rectangle
+
+That create a class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    class rectangle to defines a rectangle
+    """
+    def __init__(self):
+        self
