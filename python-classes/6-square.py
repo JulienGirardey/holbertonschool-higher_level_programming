1aec8e4f31d76c5be7105701f0b559cae74d742c
JulienGirardey <juliengirardey2@gmail.com>
2025-05-20 09:20:06 +0200

    that add position parameter in to class square

---
+++ python-classes/6-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple(int, int)):
+            TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for col in range(self.position[1]):
+            print("")
+        for i in range(self.size):
+            for row in range(self.position[0]):
+                print(" ", end="")
+            for j in range(self.size):
+                print("#", end="")
+            print()
