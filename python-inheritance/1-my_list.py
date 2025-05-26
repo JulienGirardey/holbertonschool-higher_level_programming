02d06a016e4b6f8951ace206e6011c0d12c23578
JulienGirardey <juliengirardey2@gmail.com>
2025-05-26 14:52:53 +0200

    add commente in too my method print_sorted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,10 +8,12 @@ creat class that inherit from list
 
 class MyList(list):
     """
-    class MyList
+    class MyList with a function
+    to sort a list
     """
     def print_sorted(self):
         """
-        that sort the actual list
+        that sort the actual list and
+        print it
         """
         print(sorted(self))
