11cb850aff5986453526bb90d7fd2442d69099ae
JulienGirardey <juliengirardey2@gmail.com>
2025-05-26 15:07:10 +0200

    used with my return the method issubclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     """
     return true if obj have not the same type as a_class
     """
-    return type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
