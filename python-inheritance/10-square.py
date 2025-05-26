4de5234ee6979e4853f5064231d785ae0a424a22
JulienGirardey <juliengirardey2@gmail.com>
2025-05-26 14:10:49 +0200

    creat new class Square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+square
+
+this file contains the class Square
+"""
+
+
+class Square(Rectangle):
+
+	def __init__(self, size):
+		self.integer_validator("size", size)
+		super().__init__(size, size)
+		self.__size = size
+	
+	def area(self):
+		return self.__size ** 2
