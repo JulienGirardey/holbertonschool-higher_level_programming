dc8e2b5ef5a50d442bcf4da1a4550d9ee592cc0c
JulienGirardey <juliengirardey2@gmail.com>
2025-05-26 14:26:48 +0200

    delete import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 rectangle
 
@@ -7,6 +6,21 @@ this file contains class Rectangle that
 inherits form BaseGeometry
 """
 
+
+class BaseGeometry:
+    """
+    class BaseGeometry with area method
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
 
