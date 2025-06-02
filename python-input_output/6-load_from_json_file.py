3d83479c0cf6725422d02711003faff7d49c85d0
JulienGirardey <juliengirardey2@gmail.com>
2025-06-02 12:45:30 +0200

    this function return an object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+load_from_json_file
+
+this module contains
+load_from_json_file function
+"""
+import json
+
+
+def load_from_json_file(filename):
+	"""
+	this function return an object
+	from a JSON file
+	"""
+	with open(filename, "r") as file:
+		return json.load(file)
