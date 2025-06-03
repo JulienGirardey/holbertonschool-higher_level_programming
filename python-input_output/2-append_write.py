ea2ffc688c53e534881976f16688b177b906234b
JulienGirardey <juliengirardey2@gmail.com>
2025-06-03 09:46:24 +0200

    add comment at this file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """
+append_write
+
+this module contains the
+append_write function
 """
 
 
 def append_write(filename="", text=""):
     """
+    this function append a text
+    at file and return the length
+    of the text append
     """
     with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
