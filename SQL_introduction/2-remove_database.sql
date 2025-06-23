283fbe6caa9d08156d86df1d5568d0370518db45
JulienGirardey <juliengirardey2@gmail.com>
2025-06-23 14:43:29 +0200

    fix: update SQL scripts for database removal and table listing

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- that deletes the database hbtn_0c_0
 -- in your MySQL server
-drop database hbtn_0c_0;
+drop database if exists hbtn_0c_0;
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,2 @@
 -- that lists all rows of the table first_table
-use hbtn_0c_0;
 select * from first_table;
