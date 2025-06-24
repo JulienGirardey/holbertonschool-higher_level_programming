0d51582d0832ba2faa19b8c8bce3ac9890172d37
JulienGirardey <juliengirardey2@gmail.com>
2025-06-24 16:35:42 +0200

    fix: correct SQL query to properly join states and cities for California cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- that can be found in the database hbtn_0d_usa
 SELECT cities.id, cities.name
 FROM states, cities
-WHERE cities.id = states.state_id AND states.name = 'California'
+WHERE states.id = cities.state_id AND states.name = 'California'
 ORDER BY cities.id;
