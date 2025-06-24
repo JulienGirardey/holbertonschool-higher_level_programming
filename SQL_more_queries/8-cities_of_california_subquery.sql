72db7738ecd3aea0e7757066bc7b91a8fcf5699a
JulienGirardey <juliengirardey2@gmail.com>
2025-06-24 16:34:23 +0200

    fix: correct SQL query to select city IDs instead of state IDs for California cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- that lists all the cities of California
 -- that can be found in the database hbtn_0d_usa
-SELECT states.id, cities.name
+SELECT cities.id, cities.name
 FROM states, cities
-WHERE states.id = cities.state_id AND states.name = 'California'
+WHERE cities.id = states.state_id AND states.name = 'California'
 ORDER BY cities.id;
