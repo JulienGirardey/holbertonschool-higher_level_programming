ba87a78f6e1ec6e1c8ce54f1853c838f2b2329b4
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:47:07 +0200

    style: update docstring for clarity and improve formatting in 6-model_state.py

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""
-6-model_state
-that creates the State class
-and the Base class
-for the database hbtn_0e_6_usa
-and creates the states table
-in the database
-using SQLAlchemy ORM
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -14,7 +7,9 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
