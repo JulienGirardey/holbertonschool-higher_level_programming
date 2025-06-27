3b947490988fe93a13448d84b220d06821afd624
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:55:15 +0200

    fix: update City class to include nullable and autoincrement attributes for id column

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,14 +2,14 @@
 """
 This module defines the City class which links to the MySQL table cities
 """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 
 class City(Base):
 	"""City class that represents a city in the database."""
 	__tablename__ = 'cities'
-	id = Column(Integer, primary_key=True)
-	state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+	id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
 	name = Column(String(128), nullable=False)
+	state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
