815eb667bffa75240551a47514bdad8a70ab7a6b
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:44:39 +0200

    style: format SQL query for improved readability in 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,9 +17,10 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute("""
-            SELECT cities.id, cities.name, states.name FROM cities" \
-            "INNER JOIN states ON cities.state_id = states.id" \
-            "ORDER BY cities.id ASC;
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
     """)
     rows = cursor.fetchall()
     for row in rows:
