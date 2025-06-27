2a2102df92215e0dfc043a43c1c626c67282e31c
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:53:26 +0200

    style: update module docstring for clarity and conciseness in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 """
-6-model_city
-that creates the City class
-and the Base class
-for the database hbtn_0e_6_usa
-and creates the cities table
-in the database
-using SQLAlchemy ORM
-This module defines the City class, which represents a city in the database.
-It inherits from Base and defines the table structure for cities.
-It includes columns for id, state_id, and name.
-It uses SQLAlchemy to define the database schema.
-It is used in conjunction with the model_state module to create a relationship
-between cities and states.
+This module defines the City class which links to the MySQL table cities
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
