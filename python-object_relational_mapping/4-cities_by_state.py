9c42d341081a3d13126e53df98da8bf90c1d88dc
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:40:20 +0200

    fix: add ASC to SQL query for correct ordering of cities by id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     query = "SELECT cities.id, cities.name, states.name FROM cities" \
             "INNER JOIN states ON cities.state_id = states.id" \
-            "ORDER BY cities.id;"
+            "ORDER BY cities.id ASC;"
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
