e9605c23240e6f514c12cb0a3a3a3e16715c79f3
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:56:01 +0200

    style: fix indentation in City class definition for consistency

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -8,8 +8,8 @@ from model_state import Base
 
 
 class City(Base):
-	"""City class that represents a city in the database."""
-	__tablename__ = 'cities'
-	id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-	name = Column(String(128), nullable=False)
-	state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    """City class that represents a city in the database."""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
