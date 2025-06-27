c294c2f302354221551dea1f321da31de35be147
JulienGirardey <juliengirardey2@gmail.com>
2025-06-27 16:24:31 +0200

    fix: remove autoincrement from primary key in State class definition

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,5 +13,5 @@ Base = declarative_base()
 class State(Base):
     """State class that represents a state in the database."""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
