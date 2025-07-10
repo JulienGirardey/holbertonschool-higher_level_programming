cc5c27b43899d1a6007379cb8bc6f7859e0146ec
JulienGirardey <juliengirardey2@gmail.com>
2025-07-10 15:25:24 +0200

    fix: correct string formatting in multi_languages.js for consistent output

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const C = 'C is fun\n';
-const Python = 'Python is cool\n';
+const C = 'C is fun';
+const Python = 'Python is cool';
 const js = 'JavaScript is amazing';
-console.log(C,Python, js);
+console.log(C + '\n' + Python + '\n' + js);
