7f10c39521f87a9d5e7ea3c81ea460926d0beca1
JulienGirardey <juliengirardey2@gmail.com>
2025-07-10 15:47:40 +0200

    fix: use strict equality operator in 2-arguments.js for better comparison

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const { argv } = require('node:process');
 
 if (argv.length >= 4) {
 	console.log('Arguments found');
-} else if (argv.length == 3) {
+} else if (argv.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('No argument');
