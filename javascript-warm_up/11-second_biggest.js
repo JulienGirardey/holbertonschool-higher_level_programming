1404f3a7aa9848c5df5145644a10d868c7a74e18
JulienGirardey <juliengirardey2@gmail.com>
2025-07-11 15:00:48 +0200

    feat: add 11-second_biggest.js to find the second largest number from command line arguments

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv[3] === undefined) {
+  console.log(0);
+} else {
+  let i = 2;
+  let one = 0;
+  let seconde = 0;
+  while (i < argv.length) {
+    if (one < Number(argv[i])) {
+      seconde = one;
+      one = Number(argv[i]);
+    }
+    i++;
+  }
+  console.log(seconde);
+}
