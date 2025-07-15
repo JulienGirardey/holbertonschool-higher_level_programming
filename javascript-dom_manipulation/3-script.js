f63e345eb1e6f73e4255e8da00b0d6375365ccf3
JulienGirardey <juliengirardey2@gmail.com>
2025-07-15 15:45:33 +0200

    feat: add HTML and JavaScript files for DOM manipulation exercises

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,10 @@
+header = document.querySelector("#toggle_header");
+header.addEventListener("click", function(){
+	if (header.classList.contains("red")) {
+		header.classList.remove("red");
+		header.classList.add("green");
+	} else {
+		header.classList.remove("green");
+		header.classList.add("red");
+	}
+});
