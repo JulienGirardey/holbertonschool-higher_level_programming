8b0f9fbe4df2f562494be36c6ebb5736cc2aea58
JulienGirardey <juliengirardey2@gmail.com>
2025-07-18 09:52:15 +0200

    fix: correct key retrieval in items route to fetch 'items' instead of 'item'

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -19,7 +19,7 @@ def contact():
 def items():
     with open('items.json', 'r') as f:
         data = json.load(f)
-        items_list = data.get('item', [])
+        items_list = data.get('items', [])
     return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
